8b44fc2870f96deda9b8ec145724e58a15033942
ThorKild_ <pro.antoine.masia@gmail.com>
2024-02-06 16:58:09 +0100

    mise en place controller post, model post et mose en place user

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\API\PostResource;
+use App\Repositories\API\PostRepository;
+use App\Http\Requests\API\PostStoreRequest;
+use App\Http\Requests\API\PostUpdateRequest;
+
+class PostController extends Controller
+{
+    private PostRepository $postRepository;
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+
+    public function index()
+    {
+        try {
+            $data = $this->postRepository->index();
+
+            return PostResource::collection($data);
+        } catch (\Exception $e) {
+            // Handle exception
+        }
+    }
+
+    public function show($model)
+    {
+        try {
+            $data = $this->postRepository->show($model);
+            return new PostResource($data);
+        } catch (\Exception $e) {
+            // Handle exception
+        }
+    }
+
+    public function store(PostStoreRequest $request)
+    {
+        try {
+            $data = $this->postRepository->store($request->all());
+
+            return response()->json([
+                'message' => 'Model created successfully',
+                'data' => new PostResource($data)
+            ], 201);
+        } catch (\Exception $e) {
+            // Handle exception
+        }
+    }
+
+    public function update(PostUpdateRequest $request, $model)
+    {
+        try {
+            $data = $this->postRepository->update($request->all(), $model);
+
+            return response()->json([
+                'message' => 'Model updated successfully',
+                'data' => new PostResource($data)
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 404);
+        }
+    }
+
+    public function destroy($model)
+    {
+        try {
+            $this->postRepository->destroy($model);
+
+            return response()->json([
+                'message' => 'Model deleted successfully'
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 404);
+        }
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Models\API\Post;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -50,4 +53,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function posts() : HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
