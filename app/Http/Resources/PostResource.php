95ea4e4c049e8cead45ff3b2e4e432c1d151ba3f
Alan <alan.hilarion@proton.me>
2024-02-15 11:19:44 +0100

    Refactor PostResource to reduce database queries
    The change in code was made to optimize the PostResource by reducing unnecessary database queries. This was achieved by refactoring the VehicleResource for the 'vehicle' key in PostResource to use a 'whenLoaded' method, which creates a closure only when 'vehicle' relationship is loaded.

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -21,7 +21,7 @@ class PostResource extends JsonResource
             'description' => $this->description,
             'price' => $this->price,
             'images' => $this->images,
-            'vehicle' => $this->whenLoaded('vehicle', fn() => new VehicleResource(Vehicle::find($this->vehicle_uuid))),
+            'vehicle' => new VehicleResource($this->whenLoaded('vehicle')),
             'published_at' => $this->published_at,
         ];
     }
