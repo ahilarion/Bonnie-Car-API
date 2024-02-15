e648815b2f78099f1c0e5f1364d41df6ef4bebb2
Alan <alan.hilarion@proton.me>
2024-02-15 11:01:35 +0100

    Update endpoint names for car and moto in PostController
    This commit changes the endpoint names '/lastMoto' and '/lastCar' to '/last-moto' and '/last-car' as per syntax convention. The new endpoint names map to the 'lastMoto' and 'lastCar' methods in the PostController which return the five latest posts for 'moto' and 'car' respectively.

--- routes/api/post.php
+++ routes/api/post.php
@@ -8,9 +8,9 @@ Route::group(['prefix' => 'post'], function () {
     Route::get('/{uuid}', 'PostController@show')->where('uuid', '[a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}');
 
     Route::get('/search/{keyword}', [PostController::class, 'search'])->where('keyword', '[a-zA-Z0-9]+');
-    Route::get('/lastMoto', [PostController::class, 'lastMoto']);
-    Route::get('/lastCar', [PostController::class, 'lastCar']);
-    
+    Route::get('/last-moto', [PostController::class, 'lastMoto']);
+    Route::get('/last-car', [PostController::class, 'lastCar']);
+
     Route::group(['middleware' => 'auth:sanctum'], function() {
         Route::post('/', 'PostController@store');
         Route::put('/{uuid}', 'PostController@update')->where('uuid', '[a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}');
